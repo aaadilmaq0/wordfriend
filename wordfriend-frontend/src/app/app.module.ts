11ee2577a13016438427d4f9980f5ffc86525e01
adilview <adil.maqusood@viewzenlabs.com>
2021-02-26 15:44:46 +0530

    added hammer js

--- wordfriend-frontend/src/app/app.module.ts
+++ wordfriend-frontend/src/app/app.module.ts
@@ -16,7 +16,21 @@ import { TrieService } from './services/trie.service';
 import { FormsModule } from '@angular/forms';
 import { SpinnerService } from './services/spinner.service';
 import { NgxSpinnerModule } from "ngx-spinner";
+import {
+  HammerGestureConfig,
+  HAMMER_GESTURE_CONFIG
+} from '@angular/platform-browser';
+import * as Hammer from 'hammerjs';
 
+export class MyHammerConfig extends HammerGestureConfig {
+  buildHammer(element: HTMLElement) {
+    const mc = new Hammer(element, {
+      touchAction: 'pan-y'
+    });
+
+    return mc;
+  }
+}
 @NgModule({
   declarations: [
     AppComponent
@@ -49,7 +63,11 @@ import { NgxSpinnerModule } from "ngx-spinner";
     AuthGuardService,
     LoginGuardService,
     TrieService,
-    SpinnerService
+    SpinnerService,
+    {
+      provide: HAMMER_GESTURE_CONFIG,
+      useClass: MyHammerConfig
+  }
   ],
   bootstrap: [AppComponent]
 })
